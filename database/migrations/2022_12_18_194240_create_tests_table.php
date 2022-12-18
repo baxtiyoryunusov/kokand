15c6e9eef6b4e9c8ebd744e25afd9321c1fd7e33
baxtiyoryunusov <90126543+baxtiyoryunusov@users.noreply.github.com>
2022-12-18 20:13:03 +0300

    name

--- database/migrations/2022_12_18_194240_create_tests_table.php
+++ database/migrations/2022_12_18_194240_create_tests_table.php
@@ -15,6 +15,7 @@ class CreateTestsTable extends Migration
     {
         Schema::create('tests', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->timestamps();
         });
     }
