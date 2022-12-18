aa8818da600cb2de62a9011144bf768aa6e8b0fc
baxtiyoryunusov <90126543+baxtiyoryunusov@users.noreply.github.com>
2022-12-18 20:42:38 +0300

    permission

--- config/app.php
+++ config/app.php
@@ -161,8 +161,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        RealRashid\SweetAlert\SweetAlertServiceProvider::class,
-
+        /*RealRashid\SweetAlert\SweetAlertServiceProvider::class,*/
+        Spatie\Permission\PermissionServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -228,7 +228,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Alert' => RealRashid\SweetAlert\Facades\Alert::class,
+      /*  'Alert' => RealRashid\SweetAlert\Facades\Alert::class,*/
 
 
     ],
